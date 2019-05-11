f926b75e1372c24a9dcd4c67e6b89ae1a4323e39
Silvio R. M. Amarante <silviouece@gmail.com>
2019-05-10 21:30:56 -0300

    Incluindo mensagem informativa que script não funciona no windows

--- eleicao/run.py
+++ eleicao/run.py
@@ -1,7 +1,12 @@
 import os
-
 import sys
+import time
 
+if sys.platform.find("win") != -1:
+    print("Não está funcionando. Quando o RMIRegistry executa em uma janela" +
+        "diferente, as aplicações não conseguem achar o rmiregistry. Por favor, executar dentro do eclipse.")
+    sys.exit()
+    
 os.system("mkdir bin")
 os.system("mkdir bin\interfaces")
 os.system("mkdir bin\server")
@@ -10,18 +15,17 @@ os.system("mkdir bin\server")
 os.system("javac src\interfaces\P.java -d bin\\")
 os.system("javac src\server\Peer.java -d bin\ -cp bin\\")
 
-if sys.platform.find("win") != -1:
-
+"""if sys.platform.find("win") != -1:
     os.system("start cmd call /k \"cd bin & java server.Peer 8888\"")
     os.system("start cmd call /k \"cd bin & java server.Peer 8889\"")
     os.system("start cmd call /k \"cd bin & java server.Peer 8890\"")
     os.system("start cmd call /k \"cd bin & java server.Peer 8891\"")
     os.system("start cmd call /k \"cd bin & java server.Peer 8892\"")
     
-else:
-    os.system("gnome-terminal -e 'bash -c \"rmiregistry; exec bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8888; exec bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8889; exec bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8890; exec bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8891; exec bash\"'")
-    os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8892; exec bash\"'")
+else:"""
+os.system("gnome-terminal -e 'bash -c \"rmiregistry; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8888; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8889; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8890; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8891; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"cd bin; java server.Peer 8892; exec bash\"'")
